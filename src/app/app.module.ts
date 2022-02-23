a7c185cd24ae560f238811f17a00aac45c8fc5ac
Jakub <kubhinio@gmail.com>
2022-02-23 22:56:19 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule
